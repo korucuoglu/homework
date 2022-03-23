0a18e8b5ccf58cee666a09ecf9b7d6bda4921a61
Halit Korucuoğlu <h.korucuoglu.16.06@gmail.com>
2022-03-23 13:53:57 +0300

    Bootcamp ilk hafta ödevleri yüklendi

---
+++ 3-c#/bootcamp/Bootcamp/Bootcamp.Homework/Program.cs
@@ -0,0 +1,21 @@
+﻿using Bootcamp.Homework.Homeworks.Week1;
+
+namespace Bootcamp.Homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * İstenilen ödevi buradan çalıştırmanız gerekmektedir. 
+             * Örnek olarak Homework1 ödevini çalıştırmak istiyorsanız tek yapmanız gereken
+             *      - Homework1.Run() demek olacaktır. 
+             * Bu sayede tek bir Console projesi içerisinde tüm ödevler yer alacak Bu da bize alandan ve zamandan tasarruf sağlayacaktır. 
+             
+             */
+
+            Homework1.Run(); // Homework(x) ödevini çalıştırır. 
+
+        }
+    }
+}
