792f822fbb0d4c13fcb876ec867f647a861d84f2
Halit Korucuoğlu <h.korucuoglu.16.06@gmail.com>
2022-04-25 15:33:38 +0300

    vue homework1 css-class-app eklendi

---
+++ frontend/vue/gurmen_bootcamp/week1/week-1-assignments/css-class-app/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <header>CSS Class App</header>
+    <div class="container css--class--app">
+      <div class="card text-center">
+        <ul class="css--palette text-center">
+          <li
+            v-for="color in colors"
+            :key="color"
+            :style="{ backgroundColor: color }"
+            @click="changeColor(color)"
+          ></li>
+        </ul>
+        <div
+          class="result-box"
+          :style="{ backgroundColor: selectedColor }"
+        ></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      colors: ['red', 'green', 'blue'],
+      selectedColor: null,
+    }
+  },
+
+  methods: {
+    changeColor(color) {
+      this.selectedColor = color
+    },
+  },
+}
+</script>
